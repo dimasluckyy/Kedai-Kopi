cc515825d3913f7dcfa1ab328d754ef3b525f3c0
dimasluckyy <135620697+dimasluckyy@users.noreply.github.com>
2023-06-06 15:15:22 +0700

    Update Tugas dimas 2.py

--- Tugas dimas 2.py
+++ Tugas dimas 2.py
@@ -1,25 +1,27 @@
-class MenuKopi:
-    def __init__(self, nama, harga):
-        self.__nama = nama
-        self.__harga = harga
-
-    def get_nama(self):
-        return self.__nama
-
-    def get_harga(self):
-        return self.__harga
-
-
-class AnandaCoffee:
+class CoffeeMenu:
     def __init__(self):
-        self.__menu_kopi = [
-            MenuKopi("ES Kopi Susu", 11000),
-            MenuKopi("ES Kopi Coklat", 12000),
-            MenuKopi("ES Kopi Hitam", 11000),
-            MenuKopi("Ice Americano", 14000)
-        ]
+        self.menu = {
+            "A": {"name": "ES Kopi Susu", "price": 11000},
+            "B": {"name": "ES Kopi Coklat", "price": 12000},
+            "C": {"name": "ES Kopi Hitam", "price": 11000},
+            "D": {"name": "Ice Americano", "price": 14000}
+        }
 
-    def tampilkan_menu(self):
+    def calculate_total_price(self, menu_choice, quantity):
+        item = self.menu.get(menu_choice.upper())
+        if item:
+            price = item["price"] * quantity
+            discount = 0
+            if quantity >= 5:
+                discount = int(price * 0.2)
+            subtotal = price - discount
+            tax = int(subtotal * 0.1)
+            total_price = subtotal + tax
+            return item["name"], price, discount, tax, total_price
+        else:
+            return "-", "-", "-", "-", "-"
+
+    def display_menu(self):
         print("""
         ==============================
         
@@ -34,48 +36,31 @@ class AnandaCoffee:
         ==============================
         """)
 
-    def pesan_menu(self):
-        pesan = input("Masukkan list abjad menu kopi = ").lower()
-        jumlah_pesan = int(input("Masukkan jumlah pesanan = "))
-
-        menu = self.__get_menu(pesan)
-        if menu is not None:
-            nama = menu.get_nama()
-            harga = menu.get_harga() * jumlah_pesan
-            ppn = int(harga * 0.1)
-
-            if jumlah_pesan >= 5:
-                diskon = int(harga * 0.2)
-                total_harga = harga - diskon + ppn
-            else:
-                diskon = 0
-                total_harga = harga + ppn
-
-            print("--------------------------")
-            print("Ananda Coffee")
-            print("--------------------------")
-            print("Menu :", nama)
-            print("Jumlah Pesan :", jumlah_pesan)
-            print("Harga :", harga)
-            print("Diskon :", diskon)
-            print("PPN :", ppn)
-            print("--------------------------")
-            print("Jumlah Bayar :", total_harga)
-            print("--------------------------")
-        else:
-            print("Menu tidak tersedia.")
-
-    def __get_menu(self, pesan):
-        for menu in self.__menu_kopi:
-            if pesan == menu.get_nama().lower():
-                return menu
-        return None
+    def display_order(self, menu_choice, quantity, name, price, discount, tax, total_price):
+        print("--------------------------")
+        print("Ananda Coffee")
+        print("--------------------------")
+        print("Menu:", name)
+        print("Jumlah Pesan:", quantity)
+        print("Harga:", price)
+        print("Diskon:", discount)
+        print("PPN:", tax)
+        print("--------------------------")
+        print("Jumlah Bayar:", total_price)
+        print("--------------------------")
 
 
-pilihan = "iya"
-ananda_coffee = AnandaCoffee()
+def main():
+    coffee_menu = CoffeeMenu()
+    choice = "Y"
+    while choice == "Y":
+        coffee_menu.display_menu()
+        menu_choice = input("Masukkan list abjad menu kopi: ")
+        quantity = int(input("Masukkan jumlah pesanan: "))
+        name, price, discount, tax, total_price = coffee_menu.calculate_total_price(menu_choice, quantity)
+        coffee_menu.display_order(menu_choice, quantity, name, price, discount, tax, total_price)
+        choice = input("Apakah Anda ingin order kembali? (Y/N): ")
 
-while pilihan == "iya":
-    ananda_coffee.tampilkan_menu()
-    ananda_coffee.pesan_menu()
-    pilihan = input("Apakah Anda ingin order kembali? (iya/tidak) = ").lower()
+
+if __name__ == "__main__":
+    main()
